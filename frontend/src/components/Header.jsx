a99b2d12ec593daef7e2cb7ecd873d85a96759e6
Aibek Minbaev <aibekmibaev050402@gmail.com>
2023-11-22 20:12:46 +0600

    Added logo, aligned items in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,27 @@
 import "./Header.css"
+import logo from "../static/logo_used.png" 
+
+console.log(logo) 
 
 
 export default function Header(){
     return(
         <div id="main-header">
             <div id="inner-header">
-                <div>
-                    <p>Logo</p>
+                <div id="header-left">
+                    <img id="logo-img" style={{height: 80}} alt="Logo" src={logo} href="/"/>
                 </div> 
-                <div>
-                    <p>Hourses </p>
+
+                <div id="header-middle">
+                    <a className="header-routes">Buy</a>
+                    <a className="header-routes">Rent</a>
+                    <a className="header-routes">Sell</a>
+                    <a className="header-routes">Find Agents</a>
+                    <a className="header-routes">About Us</a>
                 </div>
-                <div>
-                    <p> Login </p>
+
+                <div id="header-right">
+                    <button id="login-button">Login</button>
                 </div>
             </div>
         </div>
