49122a93fdb023f8ea6fa546f44c792945f83886
Aibek Minbaev <aibekmibaev050402@gmail.com>
2023-11-22 13:31:21 +0600

    Added property, header components

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import "./Header.css"
+
+
+export default function Header(){
+    return(
+        <div id="main-header">
+            <div id="inner-header">
+                <div>
+                    <p>Logo</p>
+                </div> 
+                <div>
+                    <p>Hourses </p>
+                </div>
+                <div>
+                    <p> Login </p>
+                </div>
+            </div>
+        </div>
+    )
+}
