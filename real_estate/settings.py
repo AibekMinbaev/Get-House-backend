6657773cba86b6471125858feb5d6ebd7204faef
Aibek Minbaev <aibekminbaev050402@gmail.com>
2023-12-11 01:03:44 +0600

    Removed frontend files

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,7 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'real_estate.urls'
@@ -148,5 +148,6 @@ MEDIA_URL = '/images/'
 MEDIA_ROOT = BASE_DIR / 'real_estate/static/images'  
 
 CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
+     'http://localhost:3000',
+     'https://get-your-house.netlify.app',
 ]
